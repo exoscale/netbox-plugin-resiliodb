ce1e8a66d888cb6bc213bb648136714ab14deddd
Guillaume Michalag <guillaume@exoscale.ch>
2024-11-27 13:36:27 +0100

    refactor: Remove unique_together constraint from LCAParams model

--- netbox_resiliodb/models.py
+++ netbox_resiliodb/models.py
@@ -110,7 +110,6 @@ class LCAParams(NetBoxModel):
         ]
         verbose_name = "LCA Parameters"
         verbose_name_plural = "LCA Parameters"
-        unique_together = [('content_type', 'object_id')]  # Additional enforcement
 
     def __str__(self):
         return f'LCA Params for {self.content_object}'
